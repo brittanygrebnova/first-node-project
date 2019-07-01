b5fe6a629bf10a6cf237f23c250a17e5652b2fa5
github username <github email address>
2019-07-01 12:59:13 -0400

    adds comments to passport.js

--- services/passport.js
+++ services/passport.js
@@ -3,18 +3,22 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
+// import the user model from mongoDB
 const User = mongoose.model("users");
 
+// passport takes the instance of user and saves the id
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
+// mongoDB takes the id from passport and uses it to find the full user entry in the DB
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
     done(null, user);
   });
 });
 
+// utilize passport's GoogleStrategy to get the user's google account info and set our mongoDB user's googleId to that user's google profile id through OAuth and save the user
 passport.use(
   new GoogleStrategy(
     {
@@ -23,16 +27,13 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
